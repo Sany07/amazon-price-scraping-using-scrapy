4f8133534120de5319172af4d4ab39e97b3bd64d
Ahasan Habib Sany <sany.baust@gmail.com>
2022-02-27 21:50:57 +0600

    updated css selectors & condition

--- amazonScraper/spiders/azon_bot.py
+++ amazonScraper/spiders/azon_bot.py
@@ -7,17 +7,16 @@ class AmazonBotSpider(scrapy.Spider):
     start_urls = [
 
         "https://www.amazon.com/s?k=iphone&page=1&qid=1624792241&ref=sr_pg_2"    
-        # 'http://quotes.toscrape.com/page/1/',           
         ]
 
     def parse(self, response):
         
         product=AmazonscraperItem()
         
-        for q in response.css('div.a-section'):
+        for data in response.css('div.a-section'):
 
-            name=q.css("span.a-text-normal::text").get()
-            price=q.css("span.a-price-whole::text").get()   
+            name=data.css("span.a-text-normal::text").get()
+            price=data.css("span.a-price-whole::text").get()   
 
             if name and price:
                 product["name"]=name
